1f511747881b75922cebd11398e05543d80a05db
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-07-03 02:02:54 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Very simple Telegram wrapper for [TLDR](https://github.com/tldr-pages/tldr) project
+
+Just specify [`BOT_TOKEN`](https://core.telegram.org/bots/api) environment variable in `source.sh`:
+
+```shell
+export BOT_TOKEN=YOUR_BOT_TOKEN
+```
+...and run start.sh
--- bot.ts
+++ bot.ts
@@ -1,7 +1,11 @@
 import { Bot } from "https://deno.land/x/grammy@v1.9.0/mod.ts";
 import { NotFoundError, fetchTLDR } from "./fetch-tldr.ts";
 
-const bot = new Bot(Deno.env.get("BOT_TOKEN")); // <-- place your bot token inside this string
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
+if (BOT_TOKEN === undefined) {
+  throw new Error("No `BOT_TOKEN` environment variable provided");
+}
+const bot = new Bot(BOT_TOKEN); // <-- place your bot token inside this string
 
 bot.command("start", (ctx) =>
   ctx.reply("Welcome! Type a command to get help with")
